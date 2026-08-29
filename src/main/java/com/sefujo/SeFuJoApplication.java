026570d8bc3beddc94d2882c380de1576b8c051a
hoangvu1118 <mhoang.vuduc@gmail.com>
2026-08-29 23:51:59 +0700

    Create project

---
+++ src/main/java/com/sefujo/SeFuJoApplication.java
@@ -0,0 +1,13 @@
+package com.sefujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeFuJoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeFuJoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sefujo/SeFuJoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sefujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeFuJoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
